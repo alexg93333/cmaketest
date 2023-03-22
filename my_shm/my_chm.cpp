12125428ea998045a5e6046b9702ddff23194de9
alexg93333 <34652967+alexg93333@users.noreply.github.com>
2023-03-22 16:30:22 +0100

    .

--- my_shm/my_chm.cpp
+++ my_shm/my_chm.cpp
@@ -10,67 +10,76 @@ namespace my_shm {
   int shm_unlink(const char* name);
   
   int shm_open(const char* name, int oflag, mode_t mode)
-{
-  struct stat sb;
-  if (stat("/dev/shm", &sb) == 0 && S_ISDIR(sb.st_mode))
   {
-    const char* path = "/dev/shm";
-    path += name;
-    return open(path.data(), oflag);
+    struct stat sb;
+    if (stat("/dev/shm", &sb) == 0 && S_ISDIR(sb.st_mode))
+    {
+      std::string path = "/dev/shm";
+      path += name;
+      return open(path.data(), oflag);
+    }
+    else
+    {
+      std::string path = getenv("TMPDIR") ?: "/tmp/";
+      if (path.rfind("/") == path.length() - 1)
+      {
+        path.pop_back();
+      }
+
+      path += "/shm";
+
+      char* tmp_dir = mkdtemp(path.data());
+      std::string mountpoint = tmp_dir ?: "";
+      if (mountpoint.empty())
+      {
+        if (errno != EEXIST)
+        {
+          return -1;
+        }
+        else
+        {
+          mountpoint = path;
+        }
+      }
+
+      std::string options;
+      options.resize(PATH_MAX);
+      options.resize(snprintf(&options[0], PATH_MAX - 1, "size=256M,uid=0,gid=0,mode=%d", mode));
+
+      if (mount("tmpfs", mountpoint.data(), 0, options.data()) != 0)
+      {
+        perror("Cannot create tmpfs. Using normal /tmp file.");
+      }
+
+      std::string file = mountpoint;
+      file += name;
+      return open(file.data(), oflag);
+    }
   }
-  else
+
+  int shm_unlink(const char* name)
   {
-    const char* path = getenv("TMPDIR") ?: "/tmp/";
-    if (path.rfind("/") == path.length() - 1)
+    struct stat sb;
+    if (stat("/dev/shm", &sb) == 0 && S_ISDIR(sb.st_mode))
     {
-      path.pop_back();
+      std::string path = "/dev/shm";
+      path += name;
+      return unlink(name);
     }
-
-    path += name;
-
-    char* mountpoint = mkdtemp(path.data());
-    if (!mountpoint)
+    else
     {
-      remove(mountpoint);
-      return -1;
+      std::string path = getenv("TMPDIR") ?: "/tmp/";
+      if (path.rfind("/") == path.length() - 1)
+      {
+        path.pop_back();
+      }
+
+      path += "/shm";
+      path += name;
+
+      return unmount(path.data(), 0);
     }
-
-    const char* options;
-    options.resize(PATH_MAX);
-    options.resize(snprintf(&options[0], PATH_MAX - 1, "size=256M,uid=0,gid=0,mode=%d", mode));
-
-    if (mount("tmpfs", mountpoint, 0, options.data()) != 0)
-    {
-      //rmdir(mountpoint);
-      //return -1;
-      perror("Cannot create tmpfs. Using normal /tmp file.");
-    }
-
-    return open(mountpoint, oflag);
   }
-}
-
-int shm_unlink(const char* name)
-{
-  struct stat sb;
-  if (stat("/dev/shm", &sb) == 0 && S_ISDIR(sb.st_mode))
-  {
-    const char* path = "/dev/shm";
-    path += name;
-    return unlink(name);
-  }
-  else
-  {
-    const char* path = getenv("TMPDIR") ?: "/tmp/";
-    if (path.rfind("/") == path.length() - 1)
-    {
-      path.pop_back();
-    }
-
-    path += name;
-
-    return unmount(path.data(), 0);
-  }
-}
+  
 }
 
